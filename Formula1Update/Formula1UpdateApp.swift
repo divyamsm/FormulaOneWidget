66f985fd7d80b443810e74c40d45c793a86e73ac
Divyam Mehta <divyammehta@yahoo.co.in>
2026-06-13 15:28:26 -0700

    Initial Commit

---
+++ Formula1Update/Formula1UpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formula1UpdateApp.swift
+//  Formula1Update
+//
+//  Created by Divyam Mehta on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Formula1UpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Formula1UpdateTests/Formula1UpdateTests.swift
@@ -0,0 +1,17 @@
+//
+//  Formula1UpdateTests.swift
+//  Formula1UpdateTests
+//
+//  Created by Divyam Mehta on 6/13/26.
+//
+
+import Testing
+@testable import Formula1Update
+
+struct Formula1UpdateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
